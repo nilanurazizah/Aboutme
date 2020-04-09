b99723d76fb1a05db3861a03c4c7588ab389fecd
Nila Nur Azizah <55771272+nilanurazizah@users.noreply.github.com>
2020-04-09 12:01:06 +0700

    Menambahkan no telepon dan email,menghapus alamat

--- perkenalan.php
+++ perkenalan.php
@@ -2,5 +2,6 @@
 echo 'Nama : Nila Nur Azizah <br>' ;
 echo 'Kelas : XI RPL 1 <br>';
 echo 'Asal Sekolah : SMK Taruna Bhakti Depok <br>';
-echo 'Alamat : Jl.Nangka RT 01/RW 02 <br>';
+echo 'No HP : 08997758215 <br>';
+echo 'Email : nurazizahnila10@gmail.com <br>';
 ?>
